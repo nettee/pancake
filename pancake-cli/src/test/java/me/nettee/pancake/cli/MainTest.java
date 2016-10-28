209c3da8f072b1cc0aeb679495664fc7d7c5696e
William <nettee.liu@gmail.com>
2016-10-28 21:22:19 +0800

    basic ui loop

---
+++ pancake-cli/src/test/java/me/nettee/pancake/cli/MainTest.java
@@ -0,0 +1,14 @@
+package me.nettee.pancake.cli;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MainTest {
+	
+	@Test
+	@Ignore
+	public void testMain() {
+		Main.main(null);
+	}
+
+}
